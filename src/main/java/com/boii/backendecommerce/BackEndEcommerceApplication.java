f1bb60eeb761dc9c17cac3dfc479291db2e8d2a2
Boii <yande.gangadhar@gmail.com>
2025-01-22 23:25:07 +0530

    Added readme

--- src/main/java/com/boii/backendecommerce/BackEndEcommerceApplication.java
+++ src/main/java/com/boii/backendecommerce/BackEndEcommerceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackEndEcommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackEndEcommerceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndEcommerceApplication.class, args);
+    }
 
 }
--- src/main/java/com/boii/backendecommerce/controller/ProductController.java
+++ src/main/java/com/boii/backendecommerce/controller/ProductController.java
@@ -1,4 +1,29 @@
 package com.boii.backendecommerce.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ProductController {
+    @PostMapping("/product")
+    public void createProduct() {
+
+    }
+
+    @GetMapping("/products")
+    public void getAllProducts() {
+
+    }
+    /*
+
+     */
+
+    @GetMapping("/product/{id}")
+    public void getProductByID(@PathVariable("id") Long id) {
+
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void deleteProduct(@PathVariable("id") Long id){
+
+    }
 }
