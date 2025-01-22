22708a409485e1840aaa8ee84a4ca04fb0105e68
Boii <yande.gangadhar@gmail.com>
2025-01-22 23:22:23 +0530

    Basic commits

---
+++ src/main/java/com/boii/backendecommerce/BackEndEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.boii.backendecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndEcommerceApplication.class, args);
+	}
+
+}
